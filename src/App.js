e20609c38000896cd7686fe07d47e6c7b1ae3778
buseozmen <79419178+buseozmen@users.noreply.github.com>
2023-11-10 23:18:51 +0300

    proje oluşturuldu 10/11/2023

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+
+  return (
+    <div>
+        <div className="flex-column container mt-5 justify-content-center" >
+            <div>
+                <h1>Oyuna Hoşgeldiniz</h1>
+            </div>
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
