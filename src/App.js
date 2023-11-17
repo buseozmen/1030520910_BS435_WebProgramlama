31e5e7d53a66d2f8c20c9467dff6691218c66ec4
buseozmen <79419178+buseozmen@users.noreply.github.com>
2023-11-17 16:17:20 +0300

    bazı denemeler yapıldı. 17.11.2023

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
-import Button from 'react-bootstrap/Button';
-
+import React, {useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faShoePrints} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+    const [value,setValue] = useState(0);
 
+    const handleClick = () => {
+        setValue(value+1);
+    }
   return (
-    <div>
-        <div className="flex-column container mt-5 justify-content-center" >
-            <div>
-                <h1>Oyuna Hoşgeldiniz</h1>
-            </div>
-
+    <div className="container d-flex flex-column container mt-5 align-items-center" style={{backgroundColor:"#7071E8",border:'2px solid #8DDFCB',padding:'1rem'}}>
+        <div>
+            <h1 style={{color:'#ECEE81'}}>Oyuna Hoşgeldiniz</h1>
+        </div>
+        <div>
+            <button className={<button type="button"></button>} onClick={handleClick} style={{backgroundColor:'#8DDFCB',color:'#ED9ED6',border:'2px solid #ED9ED6',padding:'5px'}}><FontAwesomeIcon icon={faShoePrints} /> <b>Adım atmak için tıklayınız.</b> <FontAwesomeIcon icon={faShoePrints} /></button>
+        </div>
+        <div>
+            <div style={{color:'#ED9ED6'}}><b>Adım sayısı: {value}</b></div>
         </div>
     </div>
   );
