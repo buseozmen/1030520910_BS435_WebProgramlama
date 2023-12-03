fa65975e9592e035f1ad673f0803a2e310ff004f
buseozmen <79419178+buseozmen@users.noreply.github.com>
2023-12-03 22:29:48 +0300

    başlangıç ekranı oluşturuldu 03/12/2023

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCandyCane, faShoePrints} from "@fortawesome/free-solid-svg-icons";
+import {
+    faFaceGrinTongueSquint, faFaceGrinWink,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-    const [value,setValue] = useState(0);
-
-    const handleClick = () => {
-        setValue(value+1);
-    }
   return (
       <div className={"d-flex align-items-center"}>
-          <div className="container d-flex flex-column container  align-items-center" style={{backgroundColor:"#7071E8",border:'2px solid #8DDFCB',padding:'2rem',borderRadius:'90px'}}>
+          <div className="container d-flex flex-column container  align-items-center" style={{backgroundColor:"black",border:'10px solid #6B48FF',padding:'2rem',borderRadius:'90px'}}>
               <div>
-                  <h1 style={{color:'#ECEE81'}}><FontAwesomeIcon icon={faCandyCane}/> Sayı Tahmin Oyuna Hoşgeldiniz! <FontAwesomeIcon icon={faCandyCane} /></h1>
+                  <h2 style={{color:'#FF0075'}}> Sayı Tahmin Oyuna Hoşgeldiniz! </h2>
               </div>
-              <div>
-                  <button className={<button type="button"></button>} onClick={handleClick} style={{backgroundColor:'#8DDFCB',color:'#ED9ED6',border:'2px solid #ED9ED6',padding:'5px'}}><FontAwesomeIcon icon={faShoePrints} /> <b>Adım atmak için tıklayınız.</b> <FontAwesomeIcon icon={faShoePrints} /></button>
-              </div>
-              <div>
-                  <div style={{color:'#ED9ED6'}}><b>Adım sayısı: {value}</b></div>
+              <div style={{marginTop:"2rem",marginBottom:"2rem"}}>
+                  <form>
+                      <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+                          <label style={{color:'#FF0075'}}><b>Adınız lütfen</b>  <FontAwesomeIcon icon={faFaceGrinWink} /> </label>
+                          <input placeholder={"Adınızı giriniz.."} style={{marginBottom:"3rem",backgroundColor:'black',color:'#FF0075',border:'4px solid #6B48FF',padding:'5px',focus:"border:none"}}/>
+                          <div style={{color:'#FF0075',marginBottom:"1rem"}}><b>Oyun Modu Seçiniz <FontAwesomeIcon icon={faFaceGrinTongueSquint} /></b></div>
+                          <div style={{display:"flex",flexDirection:"row",justifyContent:"space-between",marginBottom:"2rem"}}>
+                              <label style={{color:"#6B48FF",marginRight:"2rem"}}>
+                                  <input name={"mod"} type={"radio"} value={"uzay"}></input><b>Uzay Modu</b>
+                              </label>
+                              <label style={{color:"#6B48FF"}}>
+                                  <input name={"mod"} type={"radio"} value={"yer"}></input><b>Yer Altı Modu</b>
+                              </label>
+                          </div>
+                          <button style={{color:"#FF0075",backgroundColor:"black",border:"4px solid #6B48FF",padding:"0.5rem 3rem"}}><b>BAŞLA</b></button>
+                      </div>
+                  </form>
               </div>
           </div>
       </div>
